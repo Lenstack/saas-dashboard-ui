d3a23b12e328ad4acb8657aaf6cca16cbb3df12c
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2023-02-07 10:53:59 -0500

    First Commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import type {AppProps} from 'next/app'
+import Head from "next/head"
+
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Dashboard</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <meta name="description" content="Dashboard"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
