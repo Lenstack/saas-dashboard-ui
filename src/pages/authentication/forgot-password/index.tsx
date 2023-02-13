5af5f6309408ef42d7d20f66379eacf29d516513
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2023-02-12 19:06:24 -0500

    Add Forgot Password

--- src/pages/authentication/forgot-password/index.tsx
+++ src/pages/authentication/forgot-password/index.tsx
@@ -1,14 +1,12 @@
 import {Form} from "@/components";
 import {FormEvent} from "react";
 import Avatar from "boring-avatars";
-import Layout from "../layout";
-import {useRouter} from 'next/navigation';
+import Layout from "@/pages/authentication/layout";
 
-export default function SignIn() {
-    const router = useRouter()
+export default function ForgotPassword() {
+
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        router.push("/dashboard")
         console.log(event.target)
     }
 
@@ -25,18 +23,14 @@ export default function SignIn() {
                     </Form.Content>
                     <Form.Content className="flex flex-col gap-2.5">
                         <Form.Label htmlFor="email">Email</Form.Label>
-                        <Form.Input type="email" id="email" name="email" placeholder="Email"/>
+                        <Form.Input type="email" id="email" name="email"
+                                    placeholder="Enter your email"/>
                     </Form.Content>
                     <Form.Content className="flex flex-col gap-2.5">
-                        <Form.Label htmlFor="password">Password</Form.Label>
-                        <Form.Input type="password" id="password" name="password" placeholder="Password"/>
-                    </Form.Content>
-                    <Form.Content className="flex flex-col gap-2.5">
-                        <Form.Link to="/authentication/forgot-password">Forgot your password?</Form.Link>
-                        <Form.Button type="submit">Sign In</Form.Button>
+                        <Form.Button type="submit">Reset Password</Form.Button>
                     </Form.Content>
                     <Form.Content className="flex flex-col gap-2.5 items-center">
-                        <Form.Link to="/authentication/sign-up">Dont have an account?</Form.Link>
+                        <Form.Link to="/authentication/sign-in">- Back to Sign In?</Form.Link>
                     </Form.Content>
                 </Form>
             </section>
