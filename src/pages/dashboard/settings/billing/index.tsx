10e1b075d456f35bee114cda6a91234b0f3f208d
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2023-02-15 15:36:16 -0500

    Update Settings/Billing Component

--- src/pages/dashboard/settings/billing/index.tsx
+++ src/pages/dashboard/settings/billing/index.tsx
@@ -3,7 +3,10 @@ import {SettingsLayout} from "@/layouts";
 export default function Billing() {
     return (
         <SettingsLayout>
-            <h1>Billing</h1>
+            <section className="flex flex-col gap-2.5">
+                <h1>Billing</h1>
+                <p>Manage your billing and payment methods.</p>
+            </section>
         </SettingsLayout>
     )
 }
