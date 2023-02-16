ed2cabcc916c31949414e6d2c69671024b83a302
LEONARDO OSPINA <42081538+Lenstack@users.noreply.github.com>
2023-02-15 21:25:03 -0500

    Update index.tsx

--- src/layouts/settings/index.tsx
+++ src/layouts/settings/index.tsx
@@ -7,7 +7,7 @@ export const SettingsLayout = ({children}: ILayoutProps) => {
     return (
         <DashboardLayout>
             <div className="flex flex-col gap-5">
-                <Navbar items={SETTINGS_ROUTES} className="flex gap-2.5 overflow-x-scroll"/>
+                <Navbar items={SETTINGS_ROUTES} className="navbar overflow-x-scroll"/>
                 <section className="py-5 w-full">
                     {children}
                 </section>
